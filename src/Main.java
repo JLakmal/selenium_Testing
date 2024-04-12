6a66f982960747857b50e5b0ed66ce149450ad88
JLakmal <91797191+JLakmal@users.noreply.github.com>
2024-04-12 22:02:29 +0530

    Selenium add but not work

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.setProperty("webdriver.chrome.driver","\"D:\\BITUOM\\Bit\\3 Year\\Semester 6\\QA\\week-5 Selenium\\chromedriver-win64.zip\"");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        driver.close();
     }
 }
